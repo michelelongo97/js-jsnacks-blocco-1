343124fc38edd092f73d0c918679a218d2497c68
michelelongo97 <m.longo7x@gmail.com>
2024-12-03 11:37:46 +0100

    feat: add jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+// l'utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore
+const numberOne = prompt("Inserisci il primo numero");
+const numberTwo = prompt("Inserisci il secondo numero");
+
+if (numberOne > numberTwo) {
+    console.log(numberOne)
+} else {(numberTwo > numberOne) 
+    console.log(numberTwo)
+}
+
+
